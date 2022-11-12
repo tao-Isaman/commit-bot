2e1641e4edde200a8224bb96a6210c33299106c5
tao-Isaman <tao.isaman@gmail.com>
2022-11-13 01:29:06 +0700

    update scripts

--- bot.sh
+++ bot.sh
@@ -4,6 +4,9 @@ echo "$info" >> output.txt
 echo "$info"
 echo
 
+git config --global user.email "tao.isaman@gmail.com"
+git config --global user.name "tao-Isaman"
+
 git add .
 git commit -m "$info"
 git push origin main
