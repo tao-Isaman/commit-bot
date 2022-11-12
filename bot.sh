7ef723eaf26d47a7d82fd6465a8a6afda2ed1682
tao-Isaman <tao.isaman@gmail.com>
2022-11-13 01:44:53 +0700

    update scripts

--- bot.sh
+++ bot.sh
@@ -1,10 +1,11 @@
-
-
 info="Commit: $(date)"
 echo "$info" >> output.txt
 echo "$info"
 echo
 
-git add .
-git commit -m "$info"
+# git config --global user.email "tao.isaman@gmail.com"
+# git config --global user.name "tao-Isaman"
+
+# git add .
+# git commit -m "$info"
 
